224baf981feca14ec348d09498c1671c4bbda8a3
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-04-03 16:51:41 +0200

    - KI Klasse erstellt

---
+++ 4(1)/4(1)/KI/KI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4_1_
+{
+    /// <summary>
+    /// Die Klasse dient der Steuerung von nicht-menschlichen Mitspielern
+    /// </summary>
+    public class KI
+    {
+        /// <summary>
+        /// die KI-Stärke des Spielers (0-100)
+        /// </summary>
+        public int Staerke = 0;
+
+        /// <summary>
+        /// die ID dieses Spielers im Spiel
+        /// </summary>
+        public int SpielerID = 0;
+
+        /// <summary>
+        /// die ID dieses Spielers im Spiel
+        /// </summary>
+        /// <param name="Spiel2">ein Spielobjekt, auf dem gerechnet werden soll</param>
+        public void Rechne(Spiel Spiel2)
+        {
+            return;
+        }
+    }
+}
