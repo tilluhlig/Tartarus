96b70a41563ef418e546e7cb943dba44b213c55c
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-02-01 20:14:23 +0100

    KI-Projekt hinzugefügt

---
+++ KI/KI/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KI
+{
+    public interface IKI
+    {
+        int Aufruf(int Wuerfel);
+        void OnFailure();
+    }
+
+   abstract public class KI
+    {
+       public void Funktion()
+       {
+
+       }
+
+    }
+}
