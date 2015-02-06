634a134eb4a10f53c075d63ce6dd45d590551c42
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-02-06 11:24:07 +0100

    aktualisiert

--- 4(1)/4(1)/Properties/AssemblyInfo.cs
+++ 4(1)/4(1)/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -35,3 +36,4 @@ using System.Runtime.InteropServices;
 //
 
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("de-DE")]
